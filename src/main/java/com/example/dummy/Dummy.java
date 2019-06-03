e3145e9ce2ddf6ffbe719a246ff62c7b34301a65
Abhinav Manchanda <abhinav@abhinav.local>
2019-06-03 15:10:04 +0530

    Added a skeleton for quantities with JUnit as dependency and a dummy source and test class.

---
+++ src/main/java/com/example/dummy/Dummy.java
@@ -0,0 +1,4 @@
+package com.example.dummy;
+
+public class Dummy {
+}
---
+++ src/test/java/com/example/dummy/DummyTest.java
@@ -0,0 +1,8 @@
+package com.example.dummy;
+
+import org.junit.Test;
+
+public class DummyTest {
+    @Test
+    public void dummy(){}
+}
