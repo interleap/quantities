5a900754302f6a7132a92961f8805ba2fcd8c4ec
Abhinav Manchanda <abhinav@abhinav.local>
2019-06-05 14:36:47 +0530

    Added the ability to count vowels in a string which only has multiple vowels.

--- src/main/java/com/example/quantities/VowelCounter.java
+++ src/main/java/com/example/quantities/VowelCounter.java
@@ -2,6 +2,6 @@ package com.example.quantities;
 
 public class VowelCounter {
     public int count(String string) {
-        return 1;
+        return string.length();
     }
 }
--- src/test/java/com/example/quantities/VowelCounterTest.java
+++ src/test/java/com/example/quantities/VowelCounterTest.java
@@ -8,4 +8,9 @@ public class VowelCounterTest {
     @Test public void shouldCountCorrectlyInASingleVowelString(){
         assertEquals(1, new VowelCounter().count("e"));
     }
+
+
+    @Test public void shouldCountCorrectlyInAMultipleVowelString(){
+        assertEquals(7, new VowelCounter().count("iioueaa"));
+    }
 }
